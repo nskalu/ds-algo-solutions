9457d3043375ea622859e147ca19e644311b9fc7
Ngozi Kalu <kalungozi2@gmail.com>
2021-08-26 00:22:44 +0100

    adds solution for find median of two sorted arrays

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,9 +1,12 @@
 public class Sorting {
 
     public static void main(String[] args){
-        bubbleSort(null);
+        int[] a = {2,2,4,4};
+        int[] b = {2,2,4,4};
+        findMedianSortedArrays(a,b);
     }
 
+    //bubble sort code implementation
     public static void bubbleSort(int[] arr){
         //time complexity of bubbble sort is O(n^2)
         int[] nums = {3,5,1,2,5,6};
@@ -24,5 +27,61 @@ public class Sorting {
                 return;
             }
         }
+    }
+
+
+    //Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted
+    // arrays. The overall run time complexity should be O(log (m+n))
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int size = nums1.length + nums2.length;
+        int m = nums1.length;
+        int n = nums2.length;
+        int median = size/2;
+        boolean isEven = false;
+        if (size%2 == 0) isEven = true;
+
+        int[] set = new int[median + 1];
+
+        int count = 0;
+        int index1 = 0;
+        int index2 = 0;
+        while(median >= count && index1 < m && index2 < n){
+            if(nums1[index1] < nums2[index2]){
+                set[count] = nums1[index1];
+                index1++;
+                count++;
+                continue;
+            }
+            else if(nums1[index1] > nums2[index2]){
+                set[count] = nums2[index2];
+                index2++;
+                count++;
+                continue;
+            }
+            set[count] = nums1[index1];
+            index1++;
+            count++;
+            if(set.length > count){
+                set[count] = nums2[index2];
+                index2++;
+                count++;
+            }
+        }
+
+        while(index1 < m && median >= count){
+            set[count] = nums1[index1];
+            index1++;
+            count++;
+        }
+        while(index2 < n && median >= count){
+            set[count] = nums2[index2];
+            index2++;
+            count++;
+        }
+        if(isEven)
+            return ((double)set[median] + (double)set[median - 1])/2;
+        else
+            return (double)set[median];
     }
 }
